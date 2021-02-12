79c4e1b2948f277dcf715fe9328bac760e5568df
devinkristopher <devinkristopher@icloud.com>
2021-02-12 01:09:21 -0500

    adding expanded MiniPlayer design

--- Vivace/View/MiniPlayer.swift
+++ Vivace/View/MiniPlayer.swift
@@ -8,33 +8,94 @@
 import SwiftUI
 
 struct MiniPlayer: View {
+    var height = UIScreen.main.bounds.height / 3
     var animation: Namespace.ID
     @Binding var expand : Bool
     var body: some View {
+        
+        var safeArea = UIApplication.shared.windows.first?.safeAreaInsets
+        
         VStack {
+            Capsule()
+                .fill(Color.gray)
+                .frame(width: expand ? 60 : 0, height: expand ? 4 : 0)
+                .opacity(expand ? 1 : 0)
+                .padding(.top,expand ? safeArea?.top : 0)
+                .padding(.vertical, expand ? 30 : 0)
             
             HStack(spacing: 15) {
+                
+                if expand {
+                    Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+                }
+                
                 Image("pic")
                     .resizable()
                     .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                    .frame(width: 55, height: 55)
+                    .frame(width: expand ? height : 55, height: expand ? height: 55)
                     .cornerRadius(15)
-                Text("Lady Gaga")
-                    .font(.title2)
-                    .fontWeight(.bold)
+                if (!expand) {
+                    Text("Lady Gaga")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .matchedGeometryEffect(id: "Label", in: animation)
+                }
                 Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Image(systemName: "play.fill")
-                        .font(.title2)
-                        .foregroundColor(.primary)
-                })
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Image(systemName: "forward.fill")
-                        .font(.title2)
-                        .foregroundColor(.primary)
-                })
+                if (!expand) {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "play.fill")
+                            .font(.title2)
+                            .foregroundColor(.primary)
+                    })
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "forward.fill")
+                            .font(.title2)
+                            .foregroundColor(.primary)
+                    })
+                }
             }
             .padding(.horizontal)
+            
+            VStack {
+                
+                HStack {
+                    if (expand) {
+                        Text("Lady Gaga")
+                            .font(.title2)
+                            .foregroundColor(.primary)
+                            .fontWeight(.bold)
+                            .matchedGeometryEffect(id: "Label", in: animation)
+                    }
+                    Spacer(minLength: 0)
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "ellipsis.circle")
+                            .font(.title2)
+                            .foregroundColor(.primary)
+                    })
+                    
+                }
+                .padding()
+                .padding(.top)
+                
+                // Live String
+                HStack {
+                    Capsule()
+                        .fill(LinearGradient(gradient: .init(colors: [Color.primary.opacity(0.7), Color.primary.opacity(0.1)]), startPoint: .leading, endPoint: .trailing))
+                        .frame(height: 4)
+                    Text("LIVE")
+                        .fontWeight(.bold)
+                        .foregroundColor(.primary)
+                    Capsule()
+                        .fill(LinearGradient(gradient: .init(colors: [Color.primary.opacity(0.1), Color.primary.opacity(0.7)]), startPoint: .leading, endPoint: .trailing))
+                        .frame(height: 4)
+                    
+                }
+                .padding()
+                
+                Spacer(minLength: 0)
+            }
+            .frame(width: expand ? nil : 0, height: expand ? nil : 0)
+            .opacity(expand ? 1 : 0)
         }
         .frame(maxHeight: expand ? .infinity : 80)
         .background(
@@ -47,6 +108,7 @@ struct MiniPlayer: View {
                         expand.toggle()}
                     })
             )
+        .ignoresSafeArea()
         .offset(y: expand ? 0 : -48)
     }
 }
