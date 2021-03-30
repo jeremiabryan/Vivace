0c03ec7c85402d7309697a08ae60a8fc56e75f09
devinkristopher <devinkristopher@icloud.com>
2021-03-30 19:50:43 -0400

    creating configs for spotify api

---
+++ Vivace-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Vivace-Bridging-Header.h
+//  Vivace
+//
+//  Created by Devin Rogers on 3/30/21.
+//
+
+// #ifndef Vivace_Bridging_Header_h
+// #define Vivace_Bridging_Header_h
+
+// #endif /* Vivace_Bridging_Header_h */
+#import <SpotifyiOS/SpotifyiOS.h>
---
+++ Vivace/View/SpotifyAPI.swift
@@ -0,0 +1,8 @@
+//
+//  SpotifyAPI.swift
+//  Vivace
+//
+//  Created by Devin Rogers on 3/30/21.
+//
+
+import Foundation
