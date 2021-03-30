4871b337be6a51e848983f5cbfb0b2a53fcfa407
devinkristopher <devinkristopher@icloud.com>
2021-03-30 19:56:54 -0400

    building SpotifyAPI class

--- Vivace/View/SpotifyAPI.swift
+++ Vivace/View/SpotifyAPI.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+let spotifyClientID = NSLocalizedString("spotifyClientID", comment: "")
+let SpotifyRedirectURL = URL(string: "spotify-ios-quick-start://spotify-login-callback")!
+
+lazy var configuration = SPTConfiguration(
+  clientID: SpotifyClientID,
+  redirectURL: SpotifyRedirectURL
+)
+
+func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+  let parameters = appRemote.authorizationParameters(from: url);
+
+        if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
+            appRemote.connectionParameters.accessToken = access_token
+            self.accessToken = access_token
+        } else if let error_description = parameters?[SPTAppRemoteErrorDescriptionKey] {
+            // Show the error
+        }
+  return true
+}
+
+func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    guard let url = URLContexts.first?.url else {
+        return
+    }
+
+    let parameters = appRemote.authorizationParameters(from: url);
+
+    if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
+        appRemote.connectionParameters.accessToken = access_token
+        self.accessToken = access_token
+    } else if let error_description = parameters?[SPTAppRemoteErrorDescriptionKey] {
+        // Show the error
+    }
+}
