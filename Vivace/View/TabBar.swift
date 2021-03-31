ff8c055213e30b6520162bc652d722f5b62fbd27
devinkristopher <devinkristopher@icloud.com>
2021-03-31 03:08:38 -0400

    saving the game since we just beat the spotify access boss

--- Vivace/View/TabBar.swift
+++ Vivace/View/TabBar.swift
@@ -65,8 +65,9 @@ struct TabBar: View {
                 // Let's decorate our TabView!
                 .accentColor(.pink)
                 // This lambda/EventListener/callback is triggered when the TabView is loaded.
-                // practically speaking, anything in here will run when the app 'loads'.
+                // practically speaking, anything in onAppear() will run when the app 'loads'.
                 .onAppear() {
+                    // APPLE MUSIC ACCESS:
                     // StoreKit (SK) request to access the user's media library
                     SKCloudServiceController.requestAuthorization { (status) in
                         // if the user authorizes access,
@@ -79,6 +80,21 @@ struct TabBar: View {
             
                         }
                     }
+                    // SPOTIFY ACCESS:
+                    // This is beyond wildly unconventional design and will need to be moved to the SpotifyAPI.swift we're building to keep it clean, aside from the necessary Obj-C bindings.
+                    _ = AppDelegate().configuration
+                    let sessionManager = AppDelegate().sessionManager
+                    let scopes: SPTScope = [.userReadEmail, .userReadPrivate,
+                                                         .userReadPlaybackState, .userModifyPlaybackState,
+                                                         .userReadCurrentlyPlaying, .streaming, .appRemoteControl,
+                                                         .playlistReadCollaborative, .playlistModifyPublic, .playlistReadPrivate, .playlistModifyPrivate,
+                                                         .userLibraryModify, .userLibraryRead,
+                                                         .userTopRead, .userReadPlaybackState, .userReadCurrentlyPlaying,
+                                                         .userFollowRead, .userFollowModify,]
+                        if #available(iOS 11, *) {
+                          // Use this on iOS 11 and above to take advantage of SFAuthenticationSession
+                          sessionManager.initiateSession(with: scopes, options: .clientOnly)
+                        }
                 }
                 MiniPlayer(animation: animation, expand: $expand, currentSong: self.$currentSong)
 
